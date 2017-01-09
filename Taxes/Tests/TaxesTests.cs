28e264c4ad63e7702c5d046507f3013250c235ae
martynas mikalajunas <martynas.mikalajunas@gmail.com>
2017-01-09 19:01:35 +0200

    initial

---
+++ Taxes/Tests/TaxesTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Taxes.Tests
+{
+	[TestFixture]
+	class TaxesTests
+	{
+		private TaxProvider _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_sut = new TaxProvider();
+		}
+		[Test]
+		public void Returns_daily_tax_if_day_matche()
+		{
+			Assert.That(_sut.GetForDate(new DateTime(2016, 5, 13)), Is.EqualTo(0.3f));
+		}
+	}
+
+	public class TaxProvider
+	{
+		public float GetForDate(DateTime date)
+		{
+			return 0.3f;
+		}
+	}
+}
