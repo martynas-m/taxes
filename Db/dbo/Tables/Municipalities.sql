93899f6b256d6cbb0ab91965b1e7ddf5ecf16411
martynas mikalajunas <martynas.mikalajunas@gmail.com>
2017-01-10 00:28:53 +0200

    added db project

---
+++ Db/dbo/Tables/Municipalities.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Municipalities] (
+    [id]   INT            IDENTITY (1, 1) NOT NULL,
+    [name] NVARCHAR (250) NOT NULL,
+    CONSTRAINT [PK_municipality] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [UX_municipality_name] UNIQUE NONCLUSTERED ([name] ASC)
+);
+
---
+++ Db/dbo/Tables/Taxes.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Taxes] (
+    [Id]             INT        IDENTITY (1, 1) NOT NULL,
+    [MunicipalityId] INT        NOT NULL,
+    [TaxType]        TINYINT    NOT NULL,
+    [Tax]            FLOAT (53) NOT NULL,
+    [Start]          DATE       NOT NULL,
+    [End]            DATE       NOT NULL,
+    CONSTRAINT [PK_Tax] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TaxToMunicipality] FOREIGN KEY ([MunicipalityId]) REFERENCES [dbo].[Municipalities] ([id]),
+    CONSTRAINT [UX_Tax] UNIQUE NONCLUSTERED ([MunicipalityId] ASC, [TaxType] ASC, [Start] ASC, [End] ASC)
+);
+
